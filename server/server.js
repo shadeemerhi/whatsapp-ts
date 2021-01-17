5a386426def588fb1357e6d194b6375b781b628e
Shadee Merhi <shadmerhi@gmail.com>
2021-01-17 12:30:05 -0700

    cors installed and implemented - not working

--- server/server.js
+++ server/server.js
@@ -1,6 +1,18 @@
-const io = require('socket.io')(5000);
+const express = require('express');
+const app = express();
+// const cors = require('cors');
+const http = require('http');
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketio(server, { wsEngine: 'ws' })
+
+const PORT = 5000;
+// app.use(cors({ origin: true, credentials: true }));
 
 io.on('connection', socket => {
+
+  console.log('new connection')
 
   // the socket has a new ID with every connection, so we want to create static one
   const id = socket.handshake.query.id;
@@ -15,4 +27,8 @@ io.on('connection', socket => {
       })
     })
   })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server listening on Port ${PORT}`);
 })
