17061746c4baecacad478bdb3c58f688746bb16d
Shadee Merhi <shadmerhi@gmail.com>
2021-01-16 20:07:40 -0700

    send message form created

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,13 +1,19 @@
 import Sidebar from './Sidebar';
+import OpenConversation from './OpenConversation';
+import { useConversations } from '../contexts/ConversationsProvider';
 
 interface Props {
   id: string
 }
 
 export default function Dashboard(props: Props) {
+
+  const { selectedConversation } = useConversations();
+  
   return (
     <div className="d-flex" style={{ height: '100vh' }}>
       <Sidebar id={props.id}/>
+      {selectedConversation && <OpenConversation />}
     </div>
   )
 }
